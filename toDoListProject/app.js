1f856329921fc650bbc87a1dcbece3a4fa396c10
CorneliaKelinske <corneliakelinske@gmail.com>
2020-12-08 06:52:08 -0800

    starting to implement the 'list' option

--- toDoListProject/app.js
+++ toDoListProject/app.js
@@ -4,10 +4,12 @@ let list = [];
 while (input !== 'quit') {
     if (input === 'new'){
         let item = prompt('What would you like to add?');
-        list.push(item);
+        list.push(item);        
+    }
+    if (input === 'list') {
+        console.log("list loop");
         console.log(list);
     }
-    console.log("still here");
     input = prompt('What do you want to do?');
 }
 console.log("You quit!");
