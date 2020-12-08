be8806f8e34ef8c759943f250cdc749de8562856
CorneliaKelinske <corneliakelinske@gmail.com>
2020-12-08 06:46:38 -0800

    can now add to my list

--- toDoListProject/app.js
+++ toDoListProject/app.js
@@ -1,6 +1,12 @@
-let input = prompt('What do you want to do?')
+let input = prompt('What do you want to do?');
+let list = [];
 
 while (input !== 'quit') {
+    if (input === 'new'){
+        let item = prompt('What would you like to add?');
+        list.push(item);
+        console.log(list);
+    }
     console.log("still here");
     input = prompt('What do you want to do?');
 }
