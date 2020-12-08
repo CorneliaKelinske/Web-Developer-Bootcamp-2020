57bb62e3ccdb99565a6b291733f0d48670cda1b5
CorneliaKelinske <corneliakelinske@gmail.com>
2020-12-08 06:37:09 -0800

    successfully added my 'quit' option

---
+++ toDoListProject/app.js
@@ -0,0 +1,8 @@
+let input = prompt('What do you want to do?')
+
+while (input !== 'quit') {
+    console.log("still here");
+    input = prompt('What do you want to do?');
+}
+console.log("You quit!");
+
